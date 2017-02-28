930497a3070dd5a29e8a79cd414bb25af414d334
Hai Nguyen <hainm.comp@gmail.com>
2017-02-28 01:32:59 -0500

    try complicated test

--- install_ambertools.sh
+++ install_ambertools.sh
@@ -10,3 +10,25 @@ conda install ambertools-17.0-0.tar.bz2
 #    url='https://124-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.ejmiCgE/ambertools-build/amber-conda-bld/non-conda-install/linux-64.ambertools-17.0-0.28Feb17.H0613.tar.bz2''
 #    tar -xf linux-64.ambertools-17.0-0.28Feb17.H0613.tar.bz2
 #    source amber17/amber.sh
+
+# install source code for testing
+url2="http://ambermd.org/downloads/ambertools-dev/AmberTools17.tar.gz"
+tarfile=`python -c "url='$url2'; print(url.split('/')[-1])"`
+
+function download_ambertools(){
+    wget $url -O $tarfile
+    tar -xf $tarfile
+}
+
+amber.setup_test_folders amber17
+export AMBERHOME=`python -c "import sys; print(sys.prefix)"`
+cd $AMBERHOME/AmberTools/test
+make test.cpptraj
+make test.parmed
+make test.pytraj
+make test.pdb4amber
+cd $AMBERHOME/test/sanderapi
+make test
+cd $AMBERHOME/test
+make test.serial.sander.MM
+make test.serial.sander.QM
