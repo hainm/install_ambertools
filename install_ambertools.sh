87faff77335898ab8955b421423368b678824046
Hai Nguyen <hainm.comp@gmail.com>
2017-02-28 01:19:15 -0500

    ack, url

--- install_ambertools.sh
+++ install_ambertools.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 url='https://122-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.Rk0JNO6/ambertools-build/amber-conda-bld/linux-64/ambertools-17.0-0.tar.bz2'
+wget $url
 conda install ambertools-17.0-0.tar.bz2
